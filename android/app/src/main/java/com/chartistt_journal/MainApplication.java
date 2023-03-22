004a4eefb287d09c7b930310cfc37b02afac5f2b
shinamde <Wsxokn@#14>
2023-03-23 00:08:07 +0530

    download feature

--- android/app/src/main/java/com/chartistt_journal/MainApplication.java
+++ android/app/src/main/java/com/chartistt_journal/MainApplication.java
@@ -1,4 +1,5 @@
 package com.chartistt_journal;
+import com.rnfs.RNFSPackage; // <------- add package
 
 import android.app.Application;
 import com.facebook.react.PackageList;
@@ -24,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new RNFSPackage());
           return packages;
         }
 
