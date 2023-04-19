f9e0e2a26b2b5f985387a00372fe298232533a2b
siddharth <siddharth.vitechso@gmail.com>
2023-04-19 22:19:40 +0530

    completing otp functionality done

--- src/components/modals/InputPhone.js
+++ src/components/modals/InputPhone.js
@@ -37,7 +37,7 @@ const InputPhone = (props) => {
                       }}
                       withDarkTheme
                       withShadow
-                      autoFocus={false}
+                      autoFocus
                       containerStyle={styles.containerStyle}
                       codeTextStyle={styles.codeTextStyle}
                       textContainerStyle={styles.textContainerStyle}
