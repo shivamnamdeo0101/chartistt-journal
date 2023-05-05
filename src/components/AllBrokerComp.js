e734724f27e8ab1346398b81dc940f8885e38356
shinamde <Wsxokn@#14>
2023-05-05 20:32:26 +0530

    broker id and list issue solved

--- src/components/AllBrokerComp.js
+++ src/components/AllBrokerComp.js
@@ -31,9 +31,9 @@ const AllBrokerComp = ({ value, setValue }) => {
                     "id": "0",
                     "label": "All",
                     "value": "All",
-                    "iconLink": "https://ik.imagekit.io/lajz2ta7n/Brokers/ALL.png"
+                    "iconLink": "https://ik.imagekit.io/lajz2ta7n/Brokers/all_im.png"
                 })} style={{ backgroundColor: "transparent", margin: 5, alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
-                <Image style={{ width: 44, height: 44, borderRadius: 22, borderWidth: 2, backgroundColor: "#ccc", borderColor: value?.brokerId === "0" ? "#975bd9" : "transparent" }} source={{ uri: "https://ik.imagekit.io/lajz2ta7n/Brokers/ALL.png"}} />
+                <Image style={{ width: 44, height: 44, borderRadius: 22, borderWidth: 2, backgroundColor: "#ccc", borderColor: value?.brokerId === "0" ? "#975bd9" : "transparent" }} source={{ uri: "https://ik.imagekit.io/lajz2ta7n/Brokers/all_im.png"}} />
                 <Text style={{ fontSize: 10, color: "#fff" }}>All</Text>
 
                 <View style={{ borderRadius: 3, width: 6, height: 6, backgroundColor: value?.brokerId === "0" ? "#975bd9" : "transparent" }}>
@@ -44,7 +44,7 @@ const AllBrokerComp = ({ value, setValue }) => {
                 data?.brokerList?.map((item, index) => {
                     return (
                         <TouchableOpacity key={index} onPress={() => setDeafult(item)} style={{ backgroundColor: "transparent", margin: 5, alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
-                            <Image style={{ width: 44, height: 44, borderRadius: 22, borderWidth: 2, backgroundColor: "#ccc", borderColor: value?.brokerId === item?.id ? "#975bd9" : "transparent" }} source={{ uri: item?.iconLink }} />
+                            <Image style={{ width: 44, height: 44, borderRadius: 22, borderWidth: 2, backgroundColor: "#ccc", borderColor: value?.brokerId === item?.id ? "#975bd9" : "transparent" }} source={{ uri: item?.iconLink === "null" ? "https://ui-avatars.com/api/?uppercase=true&background=random&name="+item?.brokerName : item?.iconLink}} />
                             <Text style={{ fontSize: 10, color: "#fff" }}>{item?.brokerName?.length > 10 ? item?.brokerName?.substring(0, 10) + ".." : item?.brokerName}</Text>
 
                             <View style={{ borderRadius: 3, width: 6, height: 6, backgroundColor: value?.brokerId === item?.id ? "#975bd9" : "transparent" }}>
