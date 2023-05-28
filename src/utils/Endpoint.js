61fb22a7bf2cacd739ffc6c2fec2f57606daef9c
shinamde <Wsxokn@#14>
2023-05-28 15:45:43 +0530

    azure serverless

--- src/utils/Endpoint.js
+++ src/utils/Endpoint.js
@@ -1,5 +1,5 @@
 // export const EndPoint = 'http://localhost:5000/api/';
-export const EndPoint = 'http://20.207.67.111:5000/api/';
+export const EndPoint = 'https://chartisst.azurewebsites.net/api/';
 // export const EndPoint = 'https://journal-admin.onrender.com/api/';
 
 
