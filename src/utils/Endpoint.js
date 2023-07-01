eed1db1829efc29311a6d87adc9c01b8d201a6a2
shinamde <Wsxokn@#14>
2023-07-01 12:14:46 +0530

    endPoint updates

--- src/utils/Endpoint.js
+++ src/utils/Endpoint.js
@@ -1,6 +1,8 @@
 // export const EndPoint = 'http://localhost:5000/api/';
-export const EndPoint = 'https://chartisst.azurewebsites.net/api/';
+// export const EndPoint = 'https://chartisst.azurewebsites.net/api/';
 // export const EndPoint = 'https://journal-admin.onrender.com/api/';
+
+export const EndPoint = 'https://chartistt.azurewebsites.net/api/';
 
 
 
