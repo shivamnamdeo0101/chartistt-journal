1212a7330dacdea0b4a8b8f79fa0d8570d2ebf2d
thesarathi07 <thesarathi07@gmail.com>
2024-09-15 17:48:31 +0530

    endpoint

--- src/utils/Endpoint.js
+++ src/utils/Endpoint.js
@@ -1,6 +1,6 @@
 // export const EndPoint = 'http://localhost:5000/api/';
-// export const EndPoint = 'https://chartistt-journal.onrender.com/api/';
-export const EndPoint = 'https://chartistt-admin.onrender.com/api/';
+export const EndPoint = 'http://chartistt-journal-backend.ap-south-1.elasticbeanstalk.com/api/';
+// export const EndPoint = 'https://chartistt-admin.onrender.com/api/';
 
 
 
