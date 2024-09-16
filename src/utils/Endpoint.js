8b9c378c533e4191ef96cadf288803869ba2c029
shinamde <Wsxokn@#14>
2024-09-16 16:14:23 +0530

    endpoint

--- src/utils/Endpoint.js
+++ src/utils/Endpoint.js
@@ -1,5 +1,5 @@
 // export const EndPoint = 'http://localhost:5000/api/';
-export const EndPoint = 'http://chartistt-journal-backend.ap-south-1.elasticbeanstalk.com/api/';
+export const EndPoint = 'https://chartistt-journal.investist.in/api/';
 // export const EndPoint = 'https://chartistt-admin.onrender.com/api/';
 
 
